653af0cf2f6e02a6a7e9956f5173f0b11b86ef75
JuanPablo01 <blackstar371@outlook.com>
2024-12-02 03:00:49 -0600

    Modificacion al modulo de inventario

--- client_backup/src/pages/Inventory.jsx
+++ client_backup/src/pages/Inventory.jsx
@@ -1,12 +1,6 @@
-import alerta from '../components/img/alerta.png';
-import logoAzul from '../components/img/agua.png';
-import provedor from '../components/img/provedor.png';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
+import { FaBox } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
 
-import AreaChart from "../components/charts/AreaChart";
-import BarChart from "../components/charts/BarChart";
-import Navbar, { ItemNav } from "../components/dashboard/Navbar";
 import { useState } from "react";
 import { toast } from 'react-toastify'
 
@@ -15,427 +9,94 @@ export default function Inventory() {
   const [sectionSelected, setSectionSelected] = useState('Mermas')
 
   const data = [
-    { nombre: 'Harina', fecha: 'EN 2 DÍAS', cantidad: '30 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Azúcar', fecha: 'EN 3 DÍAS', cantidad: '15 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Sal', fecha: 'EN 5 DÍAS', cantidad: '20 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Aceite', fecha: 'EN 7 DÍAS', cantidad: '10 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Leche', fecha: 'HOY', cantidad: '25 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Café', fecha: 'EN 10 DÍAS', cantidad: '5 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Harina', fecha: 'EN 2 DÍAS', cantidad: '30 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Azúcar', fecha: 'EN 3 DÍAS', cantidad: '15 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Sal', fecha: 'EN 5 DÍAS', cantidad: '20 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Aceite', fecha: 'EN 7 DÍAS', cantidad: '10 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Leche', fecha: 'HOY', cantidad: '25 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Café', fecha: 'EN 10 DÍAS', cantidad: '5 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Harina', fecha: 'EN 2 DÍAS', cantidad: '30 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Azúcar', fecha: 'EN 3 DÍAS', cantidad: '15 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Sal', fecha: 'EN 5 DÍAS', cantidad: '20 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Aceite', fecha: 'EN 7 DÍAS', cantidad: '10 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Leche', fecha: 'HOY', cantidad: '25 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Café', fecha: 'EN 10 DÍAS', cantidad: '5 KG', proveedor: 'PROVEEDOR' },
-    { nombre: 'Harina', fecha: 'EN 2 DÍAS', cantidad: '30 KG', proveedor: 'PROVEEDOR' },
-
-  ];
+    { nombre: 'Harina', fecha: '2024-12-02', cantidad: '30', proveedor: 'PROVEEDOR', unidadMedida: 'KG' },
+    { nombre: 'Azúcar', fecha: '2024-12-20', cantidad: '15', proveedor: 'PROVEEDOR', unidadMedida: 'KG' },
+    { nombre: 'Sal', fecha: '2024-11-02', cantidad: '20', proveedor: 'PROVEEDOR', unidadMedida: 'KG' },
+    { nombre: 'Aceite', fecha: '2024-12-08', cantidad: '10', proveedor: 'PROVEEDOR', unidadMedida: 'L' },
+    { nombre: 'Leche', fecha: '2024-11-02', cantidad: '25', proveedor: 'PROVEEDOR', unidadMedida: 'L' },
+    { nombre: 'Café', fecha: '2024-12-02', cantidad: '5', proveedor: 'PROVEEDOR', unidadMedida: 'KG' },
+  ]
 
   return (
-    <div className="d-flex" style={{ minHeight: '100vh' }}>
-
-
-
-      {/* Contenido principal */}
-      <div
-        className="container mt-5 p-4 rounded"
-        style={{
-          backgroundColor: '#f8f9fa',
-          boxShadow: '0px 0px 15px rgba(0, 0, 0, 0.1)',
-          flex: 1,
-        }}
-      >
-
-        <Navbar
-          title={'Notificaciones'}
-        >
+    <>
+      <div className="w-full py-4">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap-4">
           {
-            ['Producto avencer/poco stock', 'Registro de material', 'Provedores', '...'].map(
-              (label) => {
-                return <ItemNav
-                  key={label}
-                  label={label}
-                  isSelected={sectionSelected === label}
-                  onClick={() => setSectionSelected(label)}
-                />
-              }
-            )
+            data.map((material, index) => {
+              return <Card
+                key={index}
+                nombreMaterial={material.nombre}
+                fechaCaducidad={material.fecha}
+                cantidad={material.cantidad}
+                unidadMedida={material.unidadMedida}
+                proveedor={material.proveedor}
+              />
+            })
           }
-        </Navbar>
-        <h1
-          className="text-center mb-4"
-          style={{ color: '#343a40', fontWeight: 'bold' }}
-        >
-          Inventario
-        </h1>
-        <div className="d-flex flex-wrap justify-content-center gap-4">
-          <Card style={{ width: '24rem', border: '2px solid red', borderRadius: '8px' }}>
-            <Card.Body
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <div>
-                <Card.Title style={{ textAlign: 'center' }}>
-                  {"PEPINO"}
-                </Card.Title>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'red' }}>
-                    Fecha de caducidad: {"En 1 dias"}
-                  </span>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'red' }}>Queda: {"2 PZ"}</span>
-                </div>
-                <Card.Link href="#">
-                  <img
-                    src={provedor}
-                    style={{
-                      width: '50px',
-                      height: '50px',
-                      objectFit: 'contain',
-                    }}
-                  />
-                </Card.Link>
-              </div>
-              <img
-                src={logoAzul}
-                alt="Material Referencial"
-                style={{
-                  marginLeft: '25px',
-                  borderRadius: '8px',
-                  width: '70px',
-                  height: '70px',
-                  objectFit: 'contain',
-                }}
-              />
-            </Card.Body>
-          </Card>
-          {data.map((item, index) => (
-            <Card key={index} style={{ width: '24rem', borderRadius: '8px' }}>
-              <Card.Body
-                style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                }}
-              >
-                <div>
-                  <Card.Title style={{ textAlign: 'center' }}>
-                    {item.nombre}
-                  </Card.Title>
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      width: '100%',
-                    }}
-                  >
-                    <span style={{ color: 'black' }}>
-                      Fecha de caducidad: {item.fecha}
-                    </span>
-                  </div>
-                  <div
-                    style={{
-                      display: 'flex',
-                      justifyContent: 'space-between',
-                      width: '100%',
-                    }}
-                  >
-                    <span style={{ color: 'black' }}>Queda: {item.cantidad}</span>
-                  </div>
-                  <Card.Link href="#">
-                    <img
-                      src={provedor}
-                      alt={item.proveedor}
-                      style={{
-                        width: '50px',
-                        height: '50px',
-                        objectFit: 'contain',
-                      }}
-                    />
-                  </Card.Link>
-                </div>
-                <img
-                  src={logoAzul}
-                  alt="Material Referencial"
-                  style={{
-                    marginLeft: '25px',
-                    borderRadius: '8px',
-                    width: '70px',
-                    height: '70px',
-                    objectFit: 'contain',
-                  }}
-                />
-              </Card.Body>
-            </Card>
-          ))}
-          {/* CARDS DE PRUEBA */}
-
-          <Card style={{ width: '24rem', border: '2px solid red', borderRadius: '8px' }}>
-
-            <Card.Body
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <div>
-                <Card.Title style={{ textAlign: 'center' }}>
-                  {"CHOCOLATE"}
-                </Card.Title>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'red' }}>
-                    Fecha de caducidad: {"En 2 dias"}
-                  </span>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'black' }}>Queda: {"30 KG"}</span>
-                </div>
-                <Card.Link href="#">
-                  <img
-                    src={provedor}
-
-                    style={{
-                      width: '50px',
-                      height: '50px',
-                      objectFit: 'contain',
-                    }}
-                  />
-                </Card.Link>
-              </div>
-              <img
-                src={logoAzul}
-                alt="Material Referencial"
-                style={{
-                  marginLeft: '25px',
-                  borderRadius: '8px',
-                  width: '70px',
-                  height: '70px',
-                  objectFit: 'contain',
-                }}
-              />
-            </Card.Body>
-          </Card>
-
-          {/* SEGUNDO CARD DE PRUEBA  */}
-
-          <Card style={{ width: '24rem', border: '2px solid red', borderRadius: '8px' }}>
-            <Card.Body
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <div>
-                <Card.Title style={{ textAlign: 'center' }}>
-                  {"Leche de almendra"}
-                </Card.Title>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'black' }}>
-                    Fecha de caducidad: {"En 2 semanas"}
-                  </span>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'red' }}>Queda: {"1.5 LT"}</span>
-                </div>
-                <Card.Link href="#">
-                  <img
-                    src={provedor}
-
-                    style={{
-                      width: '50px',
-                      height: '50px',
-                      objectFit: 'contain',
-                    }}
-                  />
-                </Card.Link>
-              </div>
-              <img
-                src={logoAzul}
-                alt="Material Referencial"
-                style={{
-                  marginLeft: '25px',
-                  borderRadius: '8px',
-                  width: '70px',
-                  height: '70px',
-                  objectFit: 'contain',
-                }}
-              />
-            </Card.Body>
-          </Card>
-
-          {/* tercer card de prueba */}
-
-          <Card style={{ width: '24rem', border: '2px solid red', borderRadius: '8px' }}>
-            <Card.Body
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <div>
-                <Card.Title style={{ textAlign: 'center' }}>
-                  {"PEPINO"}
-                </Card.Title>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'red' }}>
-                    Fecha de caducidad: {"En 1 dias"}
-                  </span>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'red' }}>Queda: {"2 PZ."}</span>
-                </div>
-                <Card.Link href="#">
-                  <img
-                    src={provedor}
-
-                    style={{
-                      width: '50px',
-                      height: '50px',
-                      objectFit: 'contain',
-                    }}
-                  />
-                </Card.Link>
-              </div>
-              <img
-                src={logoAzul}
-                alt="Material Referencial"
-                style={{
-                  marginLeft: '25px',
-                  borderRadius: '8px',
-                  width: '70px',
-                  height: '70px',
-                  objectFit: 'contain',
-                }}
-              />
-            </Card.Body>
-          </Card>
-
-          {/* CUARTO card de prueba */}
-
-
-          <Card style={{ width: '24rem', borderRadius: '8px' }}>
-
-            <Card.Body
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}
-            >
-              <div>
-                <Card.Title style={{ textAlign: 'center' }}>
-                  {"Maisena"}
-                </Card.Title>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'black' }}>
-                    Fecha de caducidad: {"En 2 dias"}
-                  </span>
-                </div>
-                <div
-                  style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                  }}
-                >
-                  <span style={{ color: 'black' }}>Queda: {"30 KG"}</span>
-                </div>
-                <Card.Link href="#">
-                  <img
-                    src={provedor}
-
-                    style={{
-                      width: '50px',
-                      height: '50px',
-                      objectFit: 'contain',
-                    }}
-                  />
-                </Card.Link>
-              </div>
-              <img
-                src={logoAzul}
-                alt="Material Referencial"
-                style={{
-                  marginLeft: '25px',
-                  borderRadius: '8px',
-                  width: '70px',
-                  height: '70px',
-                  objectFit: 'contain',
-                }}
-              />
-            </Card.Body>
-          </Card>
-
         </div>
       </div>
-    </div>
-  );
+      <div className="w-full">
+        <button className="btn btn-error">Agregar Merma</button>
+      </div>
+    </>
+  )
 }
 
+function Card({ nombreMaterial, fechaCaducidad, cantidad, unidadMedida, proveedor }) {
+
+  const calcularDias = () => {
+    const fechaActual = new Date()
+    const nuevaFecha = new Date(fechaCaducidad)
+
+    const diferenciaTiempo = nuevaFecha - fechaActual
+
+    console.log(Math.ceil(diferenciaTiempo / (1000 * 60 * 60 * 24)))
+
+    return Math.ceil(diferenciaTiempo / (1000 * 60 * 60 * 24))
+  }
+
+  return (
+    <>
+      <div className={`indicator w-full p-2 py-5 border rounded-lg ${calcularDias() < 0 ? 'bg-gray-100' : ''}`}>
+        {
+          calcularDias() < 0 ?
+            <span className="indicator-item indicator-center badge bg-red-300"><button className="font-semibold text-red-600">Pedir Material</button></span> :
+            calcularDias() < 6 ?
+              <span className="indicator-item indicator-center badge badge-primary"><FaBell /></span> : null
+        }
+        <div className="flex w-full">
+          <div className={calcularDias() < 0 ? 'hidden' : 'flex flex-col'}>
+            <div className="radial-progress text-primary" style={{ "--value": cantidad }} role="progressbar">
+              <FaBox />
+            </div>
+            <p className="mx-2 font-semibold">Queda: <span>{`${cantidad}${unidadMedida}`}</span></p>
+          </div>
+          <div className="text-center grow">
+            <h2 className="font-bold text-lg">{nombreMaterial}</h2>
+            <p className="font-semibold text-gray-600">Fecha de caducidad:</p>
+            {(() => {
+              const diasRestantes = Math.ceil(Math.abs(calcularDias()))
+              const mensaje =
+                calcularDias() < 0
+                  ? `Caducado hace ${diasRestantes} días`
+                  : calcularDias() == 0
+                    ? "Caduca hoy"
+                    : calcularDias() <= 6
+                      ? `Caduca en ${diasRestantes} días`
+                      : `Caduca en ${diasRestantes} días`;
+
+              const claseTexto =
+                calcularDias() < 0
+                  ? "text-red-600 font-medium"
+                  : calcularDias() <= 6
+                    ? "text-yellow-300 font-medium"
+                    : "text-green-600 font-medium";
+
+              return <p className={claseTexto}>{mensaje}</p>;
+            })()}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
