0d8b3a5dea8198e7bebcfc1dc33ecbd4653f8ec7
JuanPablo01 <blackstar371@outlook.com>
2024-12-03 00:14:57 -0600

    Se agrego la logica para pedir materiales

--- server/inventory/CQRS/InventoryCQRS.js
+++ server/inventory/CQRS/InventoryCQRS.js
@@ -1,12 +1,26 @@
-import { getAllMaterialsFromBase } from "../DAO/InventoryDAO.js"
+import { askedMaterials } from "../DAO/InventoryDAO.js"
+import { getMaterialFromApis } from "../DDD/InventoryDDD.js"
 
-export async function verifyGetMaterial(materialName) {
-    console.log(materialName)
+export async function verifyGetMaterial(materialName, apis) {
     if (!materialName || materialName.lenght < 0) return { status: 404, data: 'El nombre del material no respeta el formato o esta vacio' }
 
     try {
-        const response = await getAllMaterialsFromBase(materialName)
-        return { status: 200, data: response }
+        const response = await getMaterialFromApis(materialName, apis)
+        return response
+    } catch (error) {
+        console.error(error)
+        return { status: 500, data: 'Error de servidor, Intentelo nuevamente' }
+    }
+}
+
+export async function verifyAskForMaterials(idMaterial, quantity) {
+    if (!idMaterial || idMaterial < 0) return { status: 404, data: 'El id del material no puede estar vacio' }
+
+    if (!quantity || quantity < 0) return { status: 404, data: 'La cantidad del material no puede estar vacia o ser menor a 0' }
+
+    try {
+        const response = await askedMaterials(idMaterial, quantity)
+        return response
     } catch (error) {
         console.error(error)
         return { status: 500, data: 'Error de servidor, Intentelo nuevamente' }
--- server/inventory/DAO/InventoryDAO.js
+++ server/inventory/DAO/InventoryDAO.js
@@ -10,10 +10,9 @@ const connection = mysql.createConnection({
 export async function getAllMaterialsFromBase(materialName) {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM inventario WHERE nombre_insumo = ?', [materialName], (response, err) => {
-            if (err) {
-                resolve(err)
-            }
-            reject(response)
+            if (err) return resolve({ status: 400, data: err })
+
+            reject({ status: 200, data: response })
         })
     })
 }
@@ -21,10 +20,33 @@ export async function getAllMaterialsFromBase(materialName) {
 export async function getAllMaterials() {
     return new Promise((resolve, reject) => {
         connection.query('SELECT * FROM inventario', (response, err) => {
-            if (err) {
-                resolve(err)
-            }
-            reject(response)
+            if (err) return resolve({ status: 400, data: err })
+
+            reject({ status: 200, data: response })
+        })
+    })
+}
+
+
+//! Modificar por un procedure en la base de datos
+export async function askedMaterials(idMaterial, quiantity) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM inventario WHERE id_insumo = ?', [idMaterial], (response, err) => {
+            const materialQuantity = response.cantidad
+
+            if (err) return resolve({ status: 400, data: err })
+
+            if (quiantity > materialQuantity) return resolve({ status: 400, data: 'La cantidad del material solicitadad no debe de ser mayor a la cantidad disponible' })
+
+            if (quiantity > (materialQuantity / 2)) return resolve({ status: 400, data: 'La cantidad del material solicitadad no debe de ser mayor la mitad de la cantidad disponible' })
+
+            const newQuantity = materialQuantity - quiantity
+
+            connection.query('UPDATE inventario SET cantidad = ? WHERE id_insumo = ?', [newQuantity, idMaterial], (err) => {
+                if (err) return reject(err)
+
+                resolve({ status: 200, data: 'La cantidad se pidio de manera exitosa' })
+            })
         })
     })
 }
